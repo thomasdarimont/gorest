4857b9c3c247392c1065b8e30c9904058c1c7ede
Thomas Darimont <thomas.darimont@gmail.com>
2018-02-08 22:47:28 +0100

    Debug gitlab-ci

---
+++ build-docker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t gitlab:5000/gorest:latest .
--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-go get -v github.com/ahmetb/govvv
+echo "fetching tools..."
+go get github.com/golang/dep/cmd/dep
+go get github.com/ahmetb/govvv
+
+echo "ensure dependencies..."
+dep ensure
+
+echo "build binary..."
 CGO_ENABLED=0 GOOS=linux govvv build -o out/app
 
-docker build -t gitlab:5000/gorest:latest .
+echo "done."
