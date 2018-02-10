85de4508f594193d79b782bce7f1c9592d08b982
Thomas Darimont <thomas.darimont@gmail.com>
2018-02-10 11:50:27 +0100

    Refactor build

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,24 @@
 : "${DEPLOY_USER:=vagrant}"
 : "${DEPLOY_HOST:=test}"
 
+
+if [ "$1" = "initssh" ]; then
+  if ! grep docker /proc/1/cgroup -qa; then
+      echo "Can only be executed within a docker environment"
+      exit
+  fi
+
+  echo "init ssh"
+  echo SSH_PRIVATE_KEY=$SSH_PRIVATE_KEY
+  echo SSH_SERVER_HOSTKEYS=$SSH_SERVER_HOSTKEYS
+
+  which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+  eval $(ssh-agent -s)
+  ssh-add <(echo "$SSH_PRIVATE_KEY")
+  mkdir -p ~/.ssh && chmod 700 ~/.ssh
+  echo "$SSH_SERVER_HOSTKEYS" > ~/.ssh/known_hosts
+fi
+
 echo "Deploy to $ENV environment"
 
 ssh $DEPLOY_USER@$DEPLOY_HOST "docker stop $CONTAINER || true"
