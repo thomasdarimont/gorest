eb67442d8fde006d8a987a792e925c4de9481d70
Thomas Darimont <thomas.darimont@gmail.com>
2018-02-14 13:41:45 +0100

    Add hostname to info endpoint

--- actuator/info.go
+++ actuator/info.go
@@ -3,6 +3,7 @@ package actuator
 import (
 	"net/http"
 	"encoding/json"
+	"os"
 )
 
 //provided by govvv at build-time
@@ -12,8 +13,11 @@ type Info struct {
 	CommitSha string `json: commit_sha`
 	BuildDate string `json: build_date`
 	Version string  `json: version`
+	Hostname string `json: hostname`
 }
 
 func InfoEndpoint(w http.ResponseWriter, _ *http.Request) {
-	json.NewEncoder(w).Encode(&Info{CommitSha: GitCommit, BuildDate: BuildDate, Version: Version})
+
+	hostname, _ := os.Hostname()
+	json.NewEncoder(w).Encode(&Info{CommitSha: GitCommit, BuildDate: BuildDate, Version: Version, Hostname: hostname})
 }
